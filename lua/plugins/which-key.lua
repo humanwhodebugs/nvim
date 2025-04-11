b3369b1868f0ca0bbbbc5118a092544a4ed61654
Muhammad Rifai <humanwhodebugs@gmail.com>
2025-04-11 07:24:00 +0700

    Added which-key.lua
    Added it just because I want to change the border color lmao.

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    win = {
+      border = "single",
+      title = true,
+      title_pos = "center",
+      zindex = 1000,
+    },
+  },
+}
