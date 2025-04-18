abf87fb541016e7a74946504f2882c367329409e
Muhammad Rifai <humanwhodebugs@gmail.com>
2025-04-18 18:43:27 +0700

    Small Tweaks
    Changed input position from floating into default Neovim style. Only
    took me an hour to figure this thing out, great.

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -19,11 +19,26 @@ return {
         },
         presets = {
           bottom_search = true, -- Place the command-line search at the bottom
-          command_palette = true, -- Combine input for command and search
+          command_palette = false, -- Combine input for command and search
           long_message_to_split = true, -- Split long messages into a new window
         },
         cmdline = {
           view = "cmdline", -- Use cmdline view for command-line input
+          format = {
+            -- conceal: (default=true) This will hide the text in the cmdline that matches the pattern.
+            -- view: (default is cmdline view)
+            -- opts: any options passed to the view
+            -- icon_hl_group: optional hl_group for the icon
+            -- title: set to anything or empty string to hide
+            cmdline = { pattern = "^:", icon = "", lang = "vim" },
+            search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
+            search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+            filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+            lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
+            help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
+            input = { view = "cmdline", icon = "󰥻 " }, -- Used by input()
+            -- lua = false, -- to disable a format, set to `false`
+          },
         },
       })
     end,
